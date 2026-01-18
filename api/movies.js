a1564f13005b0a65d73d82027b45f07752112f0d
Rwallace0718 <65735366+Rwallace0718@users.noreply.github.com>
2026-01-18 06:56:42 -0600

    Update movies.js

--- api/movies.js
+++ api/movies.js
@@ -2,17 +2,21 @@ export default async function handler(req, res) {
     const { query, genre, id, append } = req.query;
     const TMDB_KEY = process.env.TMDB_API_KEY;
 
-    let url = `https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_KEY}&language=en-US`;
+    let url = "";
 
-    // 1. If we are asking for a SPECIFIC movie's details (Trailers/Streaming)
+    // 1. Check for specific Movie ID (Trailers/Details)
     if (id) {
         url = `https://api.themoviedb.org/3/movie/${id}?api_key=${TMDB_KEY}&append_to_response=${append}`;
     } 
-    // 2. If we are browsing by GENRE ID
+    // 2. Check for Trending request
+    else if (query === 'trending') {
+        url = `https://api.themoviedb.org/3/trending/movie/week?api_key=${TMDB_KEY}`;
+    }
+    // 3. Check for Genre ID
     else if (genre) {
         url = `https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_KEY}&with_genres=${genre}&sort_by=popularity.desc`;
     } 
-    // 3. If we are doing a TEXT SEARCH
+    // 4. Standard Keyword Search
     else if (query) {
         url = `https://api.themoviedb.org/3/search/movie?api_key=${TMDB_KEY}&query=${encodeURIComponent(query)}`;
     }
